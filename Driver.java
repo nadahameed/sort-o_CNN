67d3706c7bceb42327b1a5c2f0ed08f04bc31816
Nada <nhameed30@stuy.edu>
2022-01-06 12:32:08 -0500

    a crime

---
+++ Driver.java
@@ -0,0 +1,54 @@
+public class Driver{
+  public static void main(String[] args) {
+
+    //Bubble sort
+    ArrayList glen0 = new ArrayList<Integer>();
+    glen0.add(7);
+    glen0.add(1);
+    glen0.add(5);
+    glen0.add(12);
+    glen0.add(3);
+    System.out.println( "\nArrayList glen0 before sorting:\n" + glen0 );
+    Sorts.bubble(glen0);
+    System.out.println( "\nArrayList glen0 after sorting:\n" + glen0 );
+
+    ArrayList coco0 = populate( 10, 1, 1000 );
+    System.out.println( "\nArrayList coco0 before sorting:\n" + coco0 );
+    Sorts.bubble(coco0);
+    System.out.println( "\nArrayList coco0 after sorting:\n" + coco0 );
+
+    //Selection sort
+    ArrayList glen1 = new ArrayList<Integer>();
+    glen1.add(7);
+    glen1.add(1);
+    glen1.add(5);
+    glen1.add(12);
+    glen1.add(3);
+    System.out.println( "\nArrayList glen1 before sorting:\n" + glen1 );
+    Sorts.selection(glen1);
+    System.out.println( "\nArrayList glen1 after sorting:\n" + glen1 );
+
+    ArrayList coco1 = populate( 10, 1, 1000 );
+    System.out.println( "\nArrayList coco1 before sorting:\n" + coco1 );
+    Sorts.selection(coco1);
+    System.out.println( "\nArrayList coco1 after sorting:\n" + coco1 );
+
+    //Insertion sort
+    ArrayList glen2 = new ArrayList<Integer>();
+    glen2.add(7);
+    glen2.add(1);
+    glen2.add(5);
+    glen2.add(12);
+    glen2.add(3);
+    System.out.println( "\nArrayList glen2 before sorting:\n" + glen2 );
+    Sorts.insertion(glen2);
+    System.out.println( "\nArrayList glen2 after sorting:\n" + glen2 );
+
+    ArrayList coco2 = populate( 10, 1, 1000 );
+    System.out.println( "\nArrayList coco2 before sorting:\n" + coco2 );
+    Sorts.insertion(coco2);
+    System.out.println( "\nArrayList coco2 after sorting:\n" + coco2 );
+
+
+  }
+}
---
+++ Sorts.java
@@ -0,0 +1,88 @@
+import java.util.ArrayList;
+
+public class Sorts{
+
+  public static ArrayList populate( int size, int lo, int hi )
+  {
+    ArrayList<Integer> retAL = new ArrayList<Integer>();
+    while( size > 0 ) {
+      //     offset + rand int on interval [lo,hi]
+      retAL.add( lo + (int)( (hi-lo+1) * Math.random() ) );
+      size--;
+    }
+    return retAL;
+  }
+
+  //randomly rearrange elements of an ArrayList
+  public static void shuffle( ArrayList al )
+  {
+    int randomIndex;
+    //setup for traversal fr right to left
+    for( int i = al.size()-1; i > 0; i-- ) {
+      //pick an index at random
+      randomIndex = (int)( (i+1) * Math.random() );
+      //swap the values at position i and randomIndex
+      al.set( i, al.set( randomIndex, al.get(i) ) );
+    }
+  }
+
+  public void bubble(ArrayList<Comparable> ow) {
+    for( int passCtr = 1; passCtr < ow.size(); passCtr++ ) {
+      System.out.println( "commencing pass #" + passCtr + "..." );
+
+      //iterate thru first to next-to-last element, comparing to next
+      for( int i = 0; i < ow.size()-1; i++ ) {
+
+        //if element at i > element at i+1, swap
+        if ( ow.get(i).compareTo(ow.get(i+1) ) > 0 )
+          ow.set( i, ow.set(i+1,ow.get(i)) );
+
+        //System.out.println(ow); //diag: show current state of list
+      }
+    }
+  }
+
+  public void selection(ArrayList<Comparable> ow) {
+    //note: this version places greatest value at rightmost end,
+
+    //maxPos will point to position of SELECTION (greatest value)
+    int maxPos;
+
+    for( int pass = ow.size()-1; pass > 0; pass-- ) {
+      System.out.println( "\nbegin pass " + (ow.size()-pass) );//diag
+      maxPos = 0;
+      for( int i = 1; i <= pass; i++ ) {
+        System.out.println( "maxPos: " + maxPos );//diag
+        System.out.println( ow );//diag
+        if ( ow.get(i).compareTo( ow.get(maxPos) ) > 0 )
+          maxPos = i;
+      }
+      ow.set( maxPos, ( ow.set( pass, ow.get(maxPos) ) ) );
+      System.out.println( "after swap: " +  ow );//diag
+    }
+  }
+
+  public void insertion(ArrayList<Comparable> ow) {
+    for( int partition = 1; partition < ow.size(); partition++ ) {
+      //partition marks first item in unsorted region
+
+      //diag:
+      System.out.println( "\npartition: " + partition + "\towset:");
+      System.out.println( ow );
+
+      //traverse sorted region from right to left
+      for( int i = partition; i > 0; i-- ) {
+
+        // "walk" the current item to where it belongs
+        // by swapping adjacent items
+        if ( ow.get(i).compareTo( ow.get(i-1) ) < 0 ) {
+          //diag:
+          System.out.println( "swap indices "+(i-1)+" & "+i+"..." );
+          ow.set( i, ow.set( i-1, ow.get(i) ) );
+        }
+        else
+          break;
+      }
+    }
+  }
+}
